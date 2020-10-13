2113a985f50ad97e45bfec348c2d055d538719db
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-10-12 17:41:03 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public string playerPrefabPath;
+    public Transform[] spawnPoints;
+    public float respawnTime;
+    public PlayerController[] players;
+
+    private int playersInGame;
+
+    //instance
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        //make proper singleton
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+        }
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    private void Start()
+    {
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabPath, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        //initialize player
+        playerObj.GetComponent<PhotonView>().RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+}
---
+++ Scripts/GameUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    public TextMeshProUGUI goldText;
+
+    //instance
+    public static GameUI instance;
+
+    private void Awake()
+    {
+        //make proper singleton
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void UpdateGoldText(int gold)
+    {
+        goldText.text = "<b>Gold:</b> " + gold;
+    }
+}
+
